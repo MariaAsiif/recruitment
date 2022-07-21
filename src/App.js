a15ae91bdb6f303750cde31af732e2b27cd733cb
Arslan Ahmed <67457531+arslanahmed777@users.noreply.github.com>
2022-07-22 02:37:11 +0500

    change calendar icon and color of accordion

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { useState, useRef, useEffect, forwardRef } from 'react'
 
 
 import './App.css';
-import { IoChevronDownOutline, IoCalendarSharp } from "react-icons/io5";
+import { IoChevronDownOutline } from "react-icons/io5";
+import { BsCalendarEvent } from "react-icons/bs";
 import Business_pic from "./assets/images/sales (2).jpg"
 import Sales_pic from "./assets/images/sales representatives.jpg"
 import Administrator_pic from "./assets/images/adminstrator.jpg"
@@ -123,7 +124,7 @@ function App() {
   // ************************* Date Picker function *********************
   const ExampleCustomInput = forwardRef(({ value, onClick }, ref) => (
     <button className="bg-white w-full h-11 border-2 border-gray-400" onClick={onClick} ref={ref}>
-      {value}<IoCalendarSharp className='inline ml-6' />
+      {value}<BsCalendarEvent className='inline ml-6' />
     </button>
   ));
 
@@ -303,6 +304,96 @@ function App() {
                 <p className='text-lg text-gray-500 mb-4 '><sup className='text-red-600 font-bold'>*</sup>Mandatory</p>
                 <button onClick={handleSubmit} className='text-white bg-gray-900 w-full px-2 py-1 text-3xl font-medium tracking-wide'>SUBMIT</button>
               </div>
+
+              {/* <div className='col-6'>
+                <div class="flex justify-center">
+                  <div>
+                    <div class="dropdown relative">
+                      <button
+                        class="  dropdown-toggle  px-6   py-2.5  bg-blue-600  text-white  font-medium   text-xs  leading-tight  uppercase  rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg active:text-white transition duration-150 ease-in-outflexitems-center whitespace-nowrap " type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown"aria-expanded="false" >
+                        Dropdown button
+                        <svg  aria-hidden="true"    focusable="false"   data-prefix="fas" data-icon="caret-down"  class="w-2 ml-2" role="img"  xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 320 512"  >
+                          <path fill="currentColor"   d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"  ></path>
+                        </svg>
+                      </button>
+                      <ul class="dropdown-menu   min-w-max  absolute   hidden bg-white  text-base
+          z-50
+          float-left
+          py-2
+          list-none
+          text-left
+          rounded-lg
+          shadow-lg
+          mt-1
+          hidden
+          m-0
+          bg-clip-padding
+          border-none
+        "
+                        aria-labelledby="dropdownMenuButton1"
+                      >
+                        <li>
+                          <a
+                            class="
+              dropdown-item
+              text-sm
+              py-2
+              px-4
+              font-normal
+              block
+              w-full
+              whitespace-nowrap
+              bg-transparent
+              text-gray-700
+              hover:bg-gray-100
+            "
+                            href="#"
+                          >Action</a
+                          >
+                        </li>
+                        <li>
+                          <a
+                            class="
+              dropdown-item
+              text-sm
+              py-2
+              px-4
+              font-normal
+              block
+              w-full
+              whitespace-nowrap
+              bg-transparent
+              text-gray-700
+              hover:bg-gray-100
+            "
+                            href="#"
+                          >Another action</a
+                          >
+                        </li>
+                        <li>
+                          <a
+                            class="
+              dropdown-item
+              text-sm
+              py-2
+              px-4
+              font-normal
+              block
+              w-full
+              whitespace-nowrap
+              bg-transparent
+              text-gray-700
+              hover:bg-gray-100
+            "
+                            href="#"
+                          >Something else here</a
+                          >
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div> */}
 
             </div>
           </div>
