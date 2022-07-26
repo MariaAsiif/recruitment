41daacd9096411e33e82c21de01bf588ec9b952c
laherasif <laherasif@gmail.com>
2022-07-26 17:16:51 +0500

    setting language

--- src/App.js
+++ src/App.js
@@ -254,9 +254,8 @@ function App() {
                     <figcaption className='text-center md:mr-1  mr-1  mt-1 text-xs text-red-600 font-semibold  '>
                       {/* Language */}
                       <div className="dropdown relative w-1/5 float-right">
-                        <button className=" w-full h-full dropdown-toggle p-2 ml-4 transition duration-150 ease-in-out flex items-center whitespace-nowrap " type="button" id="surdropdown" data-bs-toggle="dropdown" aria-expanded="false">
+                        <button className=" droplang w-full h-full dropdown-toggle p-2 ml-4 transition duration-150 ease-in-out flex items-center whitespace-nowrap " type="button" id="surdropdown" data-bs-toggle="dropdown" aria-expanded="false">
                           {language.find((s_name) => s_name === lang)}
-                          
                           <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="caret-down" className="w-3  " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
                             <path fill="currentColor" d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z" />
                           </svg>
